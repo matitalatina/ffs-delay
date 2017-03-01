0484375e779e65f5d64e490f26fcd7fac25d4594
Mattia Natali <matitalatina@gmail.com>
2017-03-01 10:16:48 +0100

    Using Q library for promises

--- ffs/api.js
+++ ffs/api.js
@@ -4,37 +4,46 @@ const request = require('request');
 const moment = require('moment');
 const Q = require('q');
 
+function toJsonQ(request) {
+  var deferred = Q.defer();
+  var json = '';
+  request
+    .on('data', (data) => json += data)
+    .on('error', (err) => deferred.reject(err))
+    .on('end', () => deferred.resolve(json));
+  return deferred.promise;
+}
+
+
 function ffsApi() {
   this.BASE_PATH = 'http://transport.opendata.ch/v1/'; 
   this.STATION_BOARD_ENDPOINT = 'stationboard';
 }
 
 ffsApi.prototype.getStationboard = function (options) {
-  var deferred = Q.defer();
-  request.get(this.BASE_PATH + this.STATION_BOARD_ENDPOINT, {qs: options}, deferred.makeNodeResolver());
-  return deferred.promise;
+  return toJsonQ(request.get(this.BASE_PATH + this.STATION_BOARD_ENDPOINT, {qs: options}));
 }
 
 
 /*** Stationboard options ***/
-function stationboardOptions(options) {
+function stationboardOptionsFactory(options) {
   this.options = options || {};
 }
 
-stationboardOptions.prototype.withDatetime = function (date) {
+stationboardOptionsFactory.prototype.withDatetime = function (date) {
   this.options.datetime = moment(date).format('YYYY-MM-DD hh:mm');
   return this;
 }
 
-stationboardOptions.prototype.withOptions = function (options) {
+stationboardOptionsFactory.prototype.withOptions = function (options) {
   this.options = _.assign(this.options, options);
   return this;
 };
 
-stationboardOptions.prototype.getOptions = function () {
+stationboardOptionsFactory.prototype.getOptions = function () {
   return this.options;
 }
 
 var exports = module.exports = {};
 exports.ffsApi = ffsApi;
-exports.stationboardOptions = stationboardOptions;
+exports.stationboardOptionsFactory = stationboardOptionsFactory;
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const ffsApi = require('./ffs/api.js').ffsApi;
-const stationboardOptions = require('./ffs/api.js').stationboardOptions;
+const stationboardOptionsFactory = require('./ffs/api.js').stationboardOptions;
 const moment = require('moment');
 
-var options = new stationboardOptions({
+var optionsFactory = new stationboardOptionsFactory({
   station: 'Chiasso',
   limit: 1
 }).withDatetime(moment());
 
-new ffsApi().getStationboard(options.getOptions())
+new ffsApi().getStationboard(optionsFactory.getOptions())
   .then((data) => console.log(JSON.parse(data.toString())));
